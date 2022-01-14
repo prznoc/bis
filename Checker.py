b398e2f77cb94d315d0964ec27d8f62d3e44b07c
prznoc <przemyslaw.nocon19@gmail.com>
2022-01-14 20:00:37 +0100

    Connect GUI with Checker

--- Checker.py
+++ Checker.py
@@ -1,141 +1,161 @@
 import numpy as np
 import pandas as pd
-import math
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import LSTM
 from tensorflow.keras.layers import GRU
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 import sys
 
 look_back = 14
 
+
 def create_dataset(dataset, look_back=1):
     dataX, dataY = [], []
-    for i in range(len(dataset)-look_back-1):
-        a = dataset[i:(i+look_back)].values
+    for i in range(len(dataset) - look_back - 1):
+        a = dataset[i:(i + look_back)].values
         dataX.append(a)
         dataY.append(dataset['f'].iloc[i + look_back])
     return np.array(dataX), np.array(dataY)
 
-def trainModel(data):
+
+def train_model(data):
     data['f'] = data['f'].astype('float32')
-    train = data[0:look_back*5].copy()
+    train = data[0:look_back * 5].copy()
     trainX, trainY = create_dataset(train, look_back)
     trainX = np.reshape(trainX, (trainX.shape[0], look_back, 2))
     model = Sequential()
-    model.add(GRU(64,input_shape=(trainX.shape[1], trainX.shape[2]),
-               return_sequences=True))
+    model.add(GRU(64, input_shape=(trainX.shape[1], trainX.shape[2]),
+                  return_sequences=True))
     model.add(GRU(32))
     model.add(Dense(1))
     model.compile(loss='mean_squared_error', optimizer='sgd')
     model.fit(trainX, trainY, epochs=100, batch_size=16, verbose=0)
     return model
 
-def predictFlow(_model,data):
-    ypred=[0]*look_back
-    #_max = np.max(data['f'])
-    for k in range(len(data)-look_back):
-        pattern = data[k:k+look_back].values
+
+def predict_flow(_model, data):
+    ypred = [0] * look_back
+    # _max = np.max(data['f'])
+    for k in range(len(data) - look_back):
+        pattern = data[k:k + look_back].values
         x = np.reshape(pattern, (1, len(pattern), 2))
         ypred.append(_model.predict(x)[0][0])
-    #ypred=[v*_max for v in ypred]
+    # ypred=[v*_max for v in ypred]
     return ypred
 
-def ApEn(U, m, r):
 
+def ApEn(U, m, r):
     def _maxdist(x_i, x_j):
         return max([abs(ua - va) for ua, va in zip(x_i, x_j)])
 
     def _phi(m):
         x = [[U[j] for j in range(i, i + m - 1 + 1)] for i in range(N - m + 1)]
         C = [len([1 for x_j in x if _maxdist(x_i, x_j) <= r]) / (N - m + 1.0) for x_i in x]
-        return (N - m + 1.0)**(-1) * sum(np.log(C))
+        return (N - m + 1.0) ** (-1) * sum(np.log(C))
 
     N = len(U)
 
     return abs(_phi(m + 1) - _phi(m))
 
-def entropyTrend(data,d):
-    etrend = [ApEn(np.multiply(data[n:n+d].values,1),2,3) for n in range(len(data)-d)]
+
+def entropy_trend(data, d):
+    etrend = [ApEn(np.multiply(data[n:n + d].values, 1), 2, 3) for n in range(len(data) - d)]
     return etrend
 
-def checkFile(filepath):
-    df = pd.read_csv(filepath)
 
-    df['date'] = pd.to_datetime(df['date'])
-    df = df.groupby(['date', 'l_ipn'], as_index=False).sum()
-    df['yday'] = df['date'].dt.dayofyear
-    df['wday'] = df['date'].dt.dayofweek
+def check_file(filepath):
+    try:
+        df = pd.read_csv(filepath)
 
-    df.head(2)
+        df['date'] = pd.to_datetime(df['date'])
+        df = df.groupby(['date', 'l_ipn'], as_index=False).sum()
+        df['yday'] = df['date'].dt.dayofyear
+        df['wday'] = df['date'].dt.dayofweek
+    except Exception as e:
+        return 'File does not comply with required format'
 
-    frames = []
-    maxes = []
+    frames = dict()
+    maxes = dict()
     ips = df['l_ipn'].unique()
     for i in ips:
-        frames.append(df[df['l_ipn'] == i])
-        maxes.append(np.max(frames[i]['f']))
+        frames[i] = (df[df['l_ipn'] == i])
+        maxes[i] = np.max(frames[i]['f'])
 
-    for i in range(len(ips)):
+    for i in ips:
         fv = [float(v) / float(maxes[i]) for v in frames[i]['f'].values]
         frames[i].loc[:, 'f'] = np.array(fv).reshape(-1, 1)
 
-    models = []
-    for i in frames:
-        models.append(trainModel(i[['f', 'wday']].copy()))
+    models = dict()
+    for i in frames.keys():
+        models[i] = train_model(frames[i][['f', 'wday']].copy())
 
-    ypred = []
-    ipf = []
+    ypred = dict()
+    ipf = dict()
 
-    f, axarray = plt.subplots(5, 2, figsize=(15, 20))
-    for i in range(len(models)):
-        ypred0 = np.multiply(predictFlow(models[i], frames[i][['f', 'wday']].copy()), maxes[i])
+    size = len(frames.keys())
+    if size < 2:
+        f, axarray = plt.subplots(1, 1, figsize=(6, 8))
+    else:
+        f, axarray = plt.subplots(size // 2, 2, figsize=(size // 2 * 3, size // 2 * 4))
+    j = 0
+    for i in frames.keys():
+        ypred0 = np.multiply(predict_flow(models[i], frames[i][['f', 'wday']].copy()), maxes[i])
         ip0f = np.multiply(frames[i]['f'], maxes[i])
-        ypred.append(ypred0)
-        ipf.append(ip0f)
+        ypred[i] = (ypred0)
+        ipf[i] = (ip0f)
 
-        axarray[i // 2, i % 2].plot(frames[i]['yday'], ip0f)
-        axarray[i // 2, i % 2].plot(frames[i]['yday'], ypred0, color='r')
-        axarray[i // 2, i % 2].set_title("Local IP " + str(i) + " Flow and prediction")
+        if size < 2:
+            axarray.plot(frames[i]['yday'], ip0f)
+            axarray.plot(frames[i]['yday'], ypred0, color='r')
+            axarray.set_title("Local IP " + str(i) + " Flow and prediction")
+        else:
+            axarray[j // 2, j % 2].plot(frames[i]['yday'], ip0f)
+            axarray[j // 2, j % 2].plot(frames[i]['yday'], ypred0, color='r')
+            axarray[j // 2, j % 2].set_title("Local IP " + str(i) + " Flow and prediction")
+        j += 1
 
     f.savefig('output/plot1.png')
 
-    corrs = []
-    for i in range(len(ipf)):
-        corr0 = pd.Series(ipf[i]).corr(pd.Series(ypred[i]))
-        corrs.append(corr0)
+    corrs = dict()
 
-    frames = []
+    for i in frames.keys():
+        corr0 = pd.Series(ipf[i]).corr(pd.Series(ypred[i]))
+        corrs[i] = (corr0)
+
+    frames = dict()
     ips = df['l_ipn'].unique()
     for i in ips:
-        frames.append(df[df['l_ipn'] == i])
+        frames[i] = (df[df['l_ipn'] == i])
 
     m = 2
     r = 3
-    ent = []
-    for ip in frames:
-        ent.append(ApEn(np.multiply(ip['f'].values, 1), m, r))
+    ent = dict()
+    for ip in frames.keys():
+        ent[ip] = (ApEn(np.multiply(frames[ip]['f'].values, 1), m, r))
 
-    columns = []
-    for i in range(len(corrs)):
-        columns.append('e' + str(i))
-    rows = ['0']
-    entropy = pd.DataFrame(data=[ent], index=rows, columns=columns)
+    size = len(frames.keys())
+    if size < 2:
+        f, axarray = plt.subplots(1, 1, figsize=(6, 8))
+    else:
+        f, axarray = plt.subplots(size // 2, 2, figsize=(size // 2 * 3, size // 2 * 4))
+    j = 0
 
-    f, axarray = plt.subplots(5, 2, figsize=(15, 20))
     days = 50
 
-    for i in range(len(frames)):
-        et0 = entropyTrend(frames[i]['f'], days)
-        axarray[i // 2, i % 2].plot(range(len(et0)), et0)
-        axarray[i // 2, i % 2].set_title("Local IP " + str(ips[i]) + " ApEn Variation")
+    for i in frames.keys():
+        et0 = entropy_trend(frames[i]['f'], days)
+        if size < 2:
+            axarray.plot(range(len(et0)), et0)
+            axarray.set_title("Local IP " + str(i) + " ApEn Variation")
+        else:
+            axarray[j // 2, j % 2].plot(range(len(et0)), et0)
+            axarray[j // 2, j % 2].set_title("Local IP " + str(ips[i]) + " ApEn Variation")
+        j += 1
 
     f.savefig('output/plot2.png')
-    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-        print(entropy)
+
+    return ent
 
 
-checkFile(sys.argv[1])
+if __name__ == '__main__':
+    check_file(sys.argv[1])
--- Interface.py
+++ Interface.py
@@ -1,7 +1,11 @@
+import shutil
 import sys
 from shutil import copyfile
 from functools import partial
+from pathlib import Path
+import warnings
 
+import Checker
 from PyQt5.QtCore import QUrl, Qt
 from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QPushButton, QFileDialog, QTextEdit
 from PyQt5.uic.properties import QtCore
@@ -11,34 +15,34 @@ class Example(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.setFixedSize(400, 260)
+        self.setFixedSize(500, 260)
         self.initUI()
         self.messages = []
-        self.check_filename = 'cs448b_ipasn.csv'
+        self.file = 'cs448b_ipasn.csv'
 
     def initUI(self):
         self.button_open = QPushButton(self)
-        self.button_open.setGeometry(40, 40, 100, 50)
+        self.button_open.setGeometry(90, 40, 100, 50)
         self.button_open.setText("Set file")
         self.button_open.clicked.connect(partial(self.__clicked_btn, 1))
 
         self.button_check = QPushButton(self)
-        self.button_check.setGeometry(150, 40, 100, 50)
+        self.button_check.setGeometry(200, 40, 100, 50)
         self.button_check.setText("Run checker")
         self.button_check.clicked.connect(partial(self.__clicked_btn, 2))
 
         self.button_listen = QPushButton(self)
-        self.button_listen.setGeometry(260, 40, 100, 50)
+        self.button_listen.setGeometry(310, 40, 100, 50)
         self.button_listen.setText("Listen network")
         self.button_listen.clicked.connect(partial(self.__clicked_btn, 3))
 
         self.button_reset = QPushButton(self)
-        self.button_reset.setGeometry(40, 200, 100, 50)
+        self.button_reset.setGeometry(90, 200, 100, 50)
         self.button_reset.setText("Restore default")
         self.button_reset.clicked.connect(partial(self.__clicked_btn, 4))
 
         self.text_box = QTextEdit(self)
-        self.text_box.setGeometry(20, 100, 360, 90)
+        self.text_box.setGeometry(20, 100, 460, 90)
         self.text_box.setReadOnly(True)
 
         self.show()
@@ -54,7 +58,8 @@ class Example(QWidget):
             self.__reset_file()
 
     def __load_file(self):
-        dlg = QFileDialog()
+        home = str(Path.home())
+        dlg = QFileDialog(self, 'Network data', home)
         dlg.setModal(True)
         source_path = dlg.getOpenFileName()[0]
         url = QUrl.fromLocalFile(source_path)
@@ -64,10 +69,24 @@ class Example(QWidget):
             self.text_box.append("Loaded file: " + url.fileName())
         except FileNotFoundError as fnt:
             pass
+        except shutil.SameFileError:
+            self.file = url.fileName()
+            self.text_box.append("Loaded file: " + url.fileName())
+            pass
 
     def __check(self):
-        print('checking')
-        self.text_box.append("checking")
+        self.text_box.append('running checker on file: ' + self.file)
+        entropy = Checker.check_file('archive/' + self.file)
+        if type(entropy) is dict:
+            for key in entropy.keys():
+                value = entropy[key]
+                print(value)
+                message = 'is not suspicious'
+                if value > 0.1:
+                    message = 'is suspicious'
+                self.text_box.append('Station with address ' + str(key) + ' ' + message + f', entropy value: {value:.3f}')
+        else:
+            self.text_box.append(entropy)
 
     def __listen(self):
         print('listening')
@@ -79,22 +98,8 @@ class Example(QWidget):
 
 
 if __name__ == '__main__':
+    warnings.filterwarnings("ignore")
     app = QApplication(sys.argv)
     ex = Example()
     sys.exit(app.exec_())
 
-'''
-        app = QApplication([])
-        window = QWidget()
-        layout = QVBoxLayout()
-        dlg = QFileDialog()
-        dlg.setFileMode(QFileDialog.AnyFile)
-        # dlg.setFilter("Text files (*.txt)")
-        self.open_button = QPushButton('Select file')
-        layout.addWidget(open_button)
-        check_button = QPushButton('Check')
-        layout.addWidget(check_button)
-        window.setLayout(layout)
-        window.show()
-        app.exec()
-        '''
