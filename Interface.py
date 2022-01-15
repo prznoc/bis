c2e91db272871611a56dba3c2dc45a9ca6f2a311
prznoc <przemyslaw.nocon19@gmail.com>
2022-01-15 18:15:19 +0100

    Option selection

--- Interface.py
+++ Interface.py
@@ -1,17 +1,80 @@
 import shutil
 import sys
 import threading
+import json
 from shutil import copyfile
 from functools import partial
 from pathlib import Path
 
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIntValidator
+
 from Checker import Checker
+from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtCore import QObject, QThread, pyqtSignal, QUrl
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QFileDialog, QTextEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QFileDialog, QTextEdit, QLabel, QMainWindow, QLineEdit, \
+    QCheckBox
 from NetLog import run_from_gui
 
 
-class Example(QWidget):
+class Option(QtWidgets.QDialog):
+
+    options = QtCore.pyqtSignal(str)
+
+    def __init__(self):
+        super(Option, self).__init__()
+        self.setWindowFlags(self.windowFlags() | Qt.Window)
+        self.setFixedSize(300, 340)
+        self.initUI()
+        self._lock = threading.Lock()
+
+    def initUI(self):
+        self.label1 = QLabel(self)
+        self.label1.setGeometry(110, 40, 180, 50)
+        self.label1.setText("Select interface")
+
+        self.label2 = QLabel(self)
+        self.label2.setGeometry(110, 100, 180, 50)
+        self.label2.setText("Number of packages")
+
+        self.label3 = QLabel(self)
+        self.label3.setGeometry(110, 160, 180, 50)
+        self.label3.setText("Output filename")
+
+        self.label4 = QLabel(self)
+        self.label4.setGeometry(110, 220, 180, 50)
+        self.label4.setText("Run Checker on completion?")
+
+        self.interface_box = QLineEdit(self)
+        self.interface_box.setGeometry(20, 55, 80, 20)
+
+        self.package_box = QLineEdit(self)
+        self.package_box.setGeometry(20, 115, 80, 20)
+        self.onlyInt = QIntValidator()
+        self.package_box.setValidator(self.onlyInt)
+
+        self.filename_box = QLineEdit(self)
+        self.filename_box.setGeometry(20, 175, 80, 20)
+
+        self.option_box = QCheckBox(self)
+        self.option_box.setGeometry(90, 235, 80, 20)
+
+        self.button_close = QPushButton(self)
+        self.button_close.setGeometry(80, 280, 100, 50)
+        self.button_close.setText("Accept")
+        self.button_close.clicked.connect(partial(self.__clicked_btn))
+
+    def __clicked_btn(self):
+        result = dict()
+        result['interface'] = self.interface_box.text()
+        result['packages'] = str(self.package_box.text())
+        result['filename'] = self.filename_box.text()
+        result['option'] = str(self.option_box.isChecked())
+
+        self.options.emit(json.dumps(result))
+        self.close()
+
+class Example(QMainWindow):
 
     def __init__(self):
         super().__init__()
@@ -20,7 +83,8 @@ class Example(QWidget):
         self.messages = []
         self.file = 'cs448b_ipasn.csv'
         self._lock = threading.Lock()
-        self.listener_thread = None
+        self.options = None
+        self.listener = None
 
     def initUI(self):
         self.button_open = QPushButton(self)
@@ -79,18 +143,38 @@ class Example(QWidget):
     def __check(self, filename):
         checker = CheckerThread(filename, self)
         checker.message.connect(self.__display_text)
-        print('checker started')
         checker.start()
 
     def __listen(self):
+        if self.listener:
+            self.__disable_listener()
+            return
         with self._lock:
             self.text_box.append("listening")
             self.text_box.repaint()
-        listener = ListenerThread('enp0s3', 1000, 'network', self, False)
-        listener.message.connect(self.__display_text)
-        listener.checker_activation.connect(self.__run_checker_from_listener)
-        print('listener started')
-        listener.start()
+        option_window = Option()
+        option_window.setWindowModality(Qt.ApplicationModal)
+        option_window.options.connect(self.__listen_thread)
+        option_window.show()
+
+    def __listen_thread(self, options):
+        options = json.loads(options)
+        print(options)
+        check_option = False
+        if options['option'] == 'True':
+            check_option = True
+        self.listener = ListenerThread(options['interface'], int(options['packages']), options['filename'], self,
+                                  check_option)
+        self.listener.message.connect(self.__display_text)
+        self.listener.checker_activation.connect(self.__run_checker_from_listener)
+        self.button_listen.setText('Stop listening')
+
+        self.listener.start()
+
+    def __disable_listener(self):
+        self.listener.stop()
+        self.listener = None
+        self.button_listen.setText('Listen network')
 
     def __reset_file(self):
         self.check_filename = 'cs448b_ipasn.csv'
@@ -111,7 +195,7 @@ class ListenerThread(QThread):
     message = pyqtSignal(str)
     checker_activation = pyqtSignal(str)
 
-    def __init__(self, interface, number, filename, parent, auto_check=True):
+    def __init__(self, interface, number, filename, parent, auto_check=False):
         QObject.__init__(self, parent)
         self.interface = interface
         self.number = number
@@ -126,13 +210,15 @@ class ListenerThread(QThread):
         while True:
             self.files_amount += 1
             iteration_number = self.files_amount
-            print('message' + str(iteration_number))
+
             output_name = self.filename + str(iteration_number) + '.csv'
             result = self.__run_listener(self.interface, self.number, output_name)
-            print(result)
             self.message.emit('listening finished, output saved to file ' + output_name)
             if result and self.auto_check:
                 self.checker_activation.emit(output_name)
+
+    def stop(self):
+        self.terminate()
 
 
 class CheckerThread(QThread):
